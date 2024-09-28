46f81306a6f1face144131a1bfa3e75cea507d5b
Artem Boldyrev <artem.boldyrev@adesso.de>
2024-09-28 13:25:16 +0200

    refactor: Introduce DI container for main program

--- src/DevBasics.CarManagement/Program.cs
+++ src/DevBasics.CarManagement/Program.cs
@@ -3,56 +3,55 @@ using DevBasics.CarManagement.Dependencies;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace DevBasics.CarManagement
+namespace DevBasics.CarManagement;
+
+internal sealed class Program
 {
-    internal sealed class Program
+    internal static async Task Main()
     {
-        internal static async Task Main()
+        var serviceCollection = new ServiceCollection();
+
+        var model = new CarRegistrationModel();
+        var configuration = new MapperConfiguration(cfg => model.CreateMappings(cfg));
+        var mapper = configuration.CreateMapper();
+
+        serviceCollection.AddSingleton(mapper);
+        serviceCollection.AddSingleton<IBulkRegistrationService, BulkRegistrationServiceMock>();
+        serviceCollection.AddSingleton<ILeasingRegistrationRepository, LeasingRegistrationRepository>();
+        serviceCollection.AddSingleton<ICarRegistrationRepository, CarRegistrationRepository>();
+        serviceCollection.AddSingleton<CarManagementSettings>();
+        serviceCollection.AddSingleton<HttpHeaderSettings>();
+        serviceCollection.AddSingleton<IKowoLeasingApiClient, KowoLeasingApiClientMock>();
+        serviceCollection.AddSingleton<ITransactionStateService, TransactionStateServiceMock>();
+        serviceCollection.AddSingleton<IRegistrationDetailService, RegistrationDetailServiceMock>();
+        serviceCollection.AddSingleton<IAppSettingsRepository, AppSettingsRepository>();
+        serviceCollection.AddSingleton<CarManagementService>();
+
+        var serviceProvider = serviceCollection.BuildServiceProvider();
+        var service = serviceProvider.GetService<CarManagementService>();
+        
+        var registerCarsModel = new RegisterCarsModel
         {
-            var model = new CarRegistrationModel();
-            var configuration = new MapperConfiguration(cnfgrtn => model.CreateMappings(cnfgrtn));
-            var mapper = configuration.CreateMapper();
-
-            var bulkRegistrationServiceMock = new BulkRegistrationServiceMock();
-            var leasingRegistrationRepository = new LeasingRegistrationRepository();
-            var carRegistrationRepositoryMock = new CarRegistrationRepository(
-                leasingRegistrationRepository,
-                bulkRegistrationServiceMock,
-                mapper);
-
-            var service = new CarManagementService(
-                mapper,
-                new CarManagementSettings(),
-                new HttpHeaderSettings(),
-                new KowoLeasingApiClientMock(),
-                new TransactionStateServiceMock(),
-                bulkRegistrationServiceMock,
-                new RegistrationDetailServiceMock(),
-                leasingRegistrationRepository,
-                carRegistrationRepositoryMock,
-                new AppSettingsRepository());
-
-            var result = await service.RegisterCarsAsync(
-                new RegisterCarsModel
+            CompanyId = "Company",
+            CustomerId = "Customer",
+            VendorId = "Vendor",
+            Cars = new List<CarRegistrationModel>
+            {
+                new CarRegistrationModel
                 {
                     CompanyId = "Company",
                     CustomerId = "Customer",
-                    VendorId = "Vendor",
-                    Cars = new List<CarRegistrationModel>
-                    {
-                        new CarRegistrationModel
-                        {
-                            CompanyId = "Company",
-                            CustomerId = "Customer",
-                            VehicleIdentificationNumber = Guid.NewGuid().ToString(),
-                            DeliveryDate = DateTime.Now.AddDays(14).Date,
-                            ErpDeliveryNumber = Guid.NewGuid().ToString()
-                        }
-                    }
-                },
-                false,
-                new Claims());
-        }
+                    VehicleIdentificationNumber = Guid.NewGuid().ToString(),
+                    DeliveryDate = DateTime.Now.AddDays(14).Date,
+                    ErpDeliveryNumber = Guid.NewGuid().ToString()
+                }
+            }
+        };
+        var result = await service.RegisterCarsAsync(
+            registerCarsModel,
+            false,
+            new Claims());
     }
 }
